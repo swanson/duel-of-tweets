ecc3cc89560ff75ed000dbad70f268f567ec271d
Matt Swanson <swanson@purdue.edu>
2010-08-14 00:12:55 -0400

    adding gitignore; storing tweets in mongodb

--- firehose.py
+++ firehose.py
@@ -1,10 +1,18 @@
 #!/usr/bin/env python
-
+from mongoengine import *
 import tweetstream
-import getpass
+from datetime import datetime
 
-words = ["@showoftweets"]
+class TweetStore(Document):
+    user = StringField(required = True)
+    body = StringField(required = True)
+    timestamp = DateTimeField()
+
+connect('testing')
+words = ["@showoftweets", "beiber"]
 stream = tweetstream.TrackStream("battleoftweets", "secretpass", words)
 for tweet in stream:
-    print "Got interesting tweet:", tweet['text']
+    print "Storing tweet %s" % tweet['text']
+    ts  = TweetStore(body = tweet['text'], user = tweet['user']['screen_name'], timestamp = datetime.now())
+    ts.save()
 
