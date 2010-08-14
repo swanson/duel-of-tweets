c4dcc3ec8bf036b5080d85d80c2913742358f055
Jevin Sweval <jevinsweval ta gmail tod com>
2010-08-13 23:42:20 -0400

    fixed tweetstream

--- firehose.py
+++ firehose.py
@@ -1,8 +1,10 @@
+#!/usr/bin/env python
+
 import tweetstream
 import getpass
 
-words = ["showoftweets", "@showoftweets"]
-stream = tweetstream.TrackStream("_swanson", getpass.getpass(), words)
+words = ["@showoftweets"]
+stream = tweetstream.TrackStream("battleoftweets", "secretpass", words)
 for tweet in stream:
     print "Got interesting tweet:", tweet['text']
 
